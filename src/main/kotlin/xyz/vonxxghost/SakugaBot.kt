3ab1211ba1eade04ec220467f056b8a4d19388b4
VonXXGhost <vonxxghost@foxmail.com>
2020-05-14 23:10:34 +0800

    添加随机作画功能

--- src/main/kotlin/xyz/vonxxghost/SakugaBot.kt
+++ src/main/kotlin/xyz/vonxxghost/SakugaBot.kt
@@ -18,8 +18,10 @@ import net.mamoe.mirai.message.data.MessageChain
 import net.mamoe.mirai.utils.BotConfiguration
 import org.jsoup.Jsoup
 import java.net.URL
+import java.time.LocalDate
 import java.util.*
 import kotlin.concurrent.schedule
+import kotlin.random.Random
 
 const val HELP_MSG = """使用指南
 在群里发送sakugabooru的稿件链接，本账号将自动搜索是否存在微博gif数据，如果存在则发送gif地址到群里，不存在就无反应。
@@ -27,24 +29,59 @@ const val HELP_MSG = """使用指南
 暂无设置功能，不想看到禁言即可。现处于新程序测试阶段，问题较多见怪莫怪。"""
 
 val log = KotlinLogging.logger("sakugaBotMain")
+var leastID = 121397L
+var lastUpdateDay = LocalDate.of(2020, 1, 1)
 
 data class SakugaTag(val type: Int, val name: String, val main_name: String)
 data class SakugaWeibo(val weibo_id: String, val img_url: String, val weibo_url: String)
-data class SakugaPost(val id: Long, val source: String?, val tags: List<SakugaTag>, val weibo: SakugaWeibo?)
+data class SakugaPost(
+    val id: Long,
+    val source: String?,
+    val tags: List<SakugaTag>,
+    val weibo: SakugaWeibo?,
+    val sakugabooru_url: String
+)
+
+data class SakugaPostSimple(val id: Long)
+data class SakugaPostList(val results: List<SakugaPostSimple>)
 
 data class TextAndUrls(val text: String, val urls: List<String>)
 
 fun getPostFromBotAPI(postID: String): String =
     "https://sakugabot.pw/api/posts/$postID/?format=json"
 
-fun getBotData(postIDs: Sequence<String>): Sequence<SakugaPost> =
+fun updateLeastPostId() {
+    val resp = Jsoup
+        .connect("https://sakugabot.pw/api/posts/?format=json")
+        .ignoreContentType(true)
+        .get()
+        .text()
+    val json = Gson().fromJson(resp, SakugaPostList::class.java)
+    leastID = json.results.first().id
+    log.info { "更新最新ID为$leastID" }
+}
+
+fun updateUpdateDay(): Boolean {
+    val now = LocalDate.now()
+    if (now.isEqual(lastUpdateDay)) {
+        return false
+    }
+    lastUpdateDay = now
+    log.info { "更新日期为$lastUpdateDay" }
+    return true
+}
+
+fun getBotData(postIDs: Sequence<String>): Sequence<SakugaPost?> =
     postIDs.map {
         val resp = Jsoup
             .connect(getPostFromBotAPI(it))
             .ignoreContentType(true)
-            .get()
-            .text()
-        return@map Gson().fromJson(resp, SakugaPost::class.java)
+            .execute()
+        if (resp.statusCode() != 200) {
+            return@map null
+        }
+        val text = resp.body()
+        return@map Gson().fromJson(text, SakugaPost::class.java)
     }
 
 
@@ -54,6 +91,30 @@ fun MessageChain.getPostIDs(): Sequence<String> {
     return results.map { it.value }.distinct()
 }
 
+fun SakugaPost.copyright(): String =
+    this.tags.filter { it.type == 3 }.joinToString("，") { it.main_name }
+
+fun SakugaPost.artist(): String =
+    this.tags.filter { it.type == 1 }.joinToString("，") { it.main_name }
+
+fun geneReplyTextAndPicUrl(id: Long): TextAndUrls? {
+    val urls = mutableListOf<String>()
+
+    val posts = getBotData(sequenceOf(id.toString()))
+    val post = posts.first()
+    if (post?.weibo == null) {
+        log.info { "Post ${post?.id} 微博数据不存在" }
+        return null
+    }
+
+    val copyright = post.copyright()
+    val artist = post.artist()
+    urls.add(post.weibo.img_url)
+
+    val replyText = "${post.sakugabooru_url}\n$copyright ${post.source} $artist\n${post.weibo.img_url}"
+
+    return TextAndUrls(replyText, urls.take(1))
+}
 
 fun geneReplyTextAndPicUrl(message: MessageChain): TextAndUrls {
     val replyText = StringBuilder()
@@ -62,18 +123,26 @@ fun geneReplyTextAndPicUrl(message: MessageChain): TextAndUrls {
     val postIDs = message.getPostIDs()
     val posts = getBotData(postIDs)
     for (post in posts) {
-        if (post.weibo != null) {
-            val copyright = post.tags.filter { it.type == 3 }.joinToString("，") { it.main_name }
-            val artist = post.tags.filter { it.type == 1 }.joinToString("，") { it.main_name }
+        if (post?.weibo != null) {
+            val copyright = post.copyright()
+            val artist = post.artist()
             urls.add(post.weibo.img_url)
             replyText.append("${post.id}:\n$copyright ${post.source} $artist\n${post.weibo.img_url}\n")
         } else {
-            log.info { "Post ${post.id} 微博数据不存在" }
+            log.info { "Post ${post?.id} 微博数据不存在" }
         }
     }
 
     // gif链接最多只取1个
     return TextAndUrls(replyText.trim().toString(), urls.take(3))
+}
+
+fun getRandomPostId(): Long {
+    var id: Long
+    do {
+        id = Random.nextLong(28799L, leastID)
+    } while (id in 88500..102000) // spam
+    return id
 }
 
 suspend fun main() {
@@ -112,6 +181,25 @@ suspend fun main() {
                     "组[${group.id}][${group.name}]人[${sender.id}][${sender.nameCardOrNick}]: " +
                             message.contentToString()
                 }
+            }
+        }
+
+        case("#随机作画") {
+            if (updateUpdateDay()) {
+                updateLeastPostId() // 每日更新
+            }
+            for (i in 0..10) {
+                val id = getRandomPostId()
+                log.info { "随机尝试id：$id" }
+                val dat = geneReplyTextAndPicUrl(id) ?: continue
+
+                if (dat.text.isNotEmpty()) {
+                    reply("Random-" + dat.text)
+                }
+                for (url in dat.urls) {
+                    sendImage(URL(url))
+                }
+                break
             }
         }
     }
